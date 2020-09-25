374023e0143e08397517b48b32d491996f65a6cc
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-09-25 00:07:54 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,3 +5,13 @@ end
 def toadstool
   puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
+
+def link
+  puts message = "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases 
+  mario
+  toadstool
+  link
+end
